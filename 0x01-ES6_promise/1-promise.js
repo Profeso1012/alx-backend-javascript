f4dd98b97576c321296ae06b3f761dd136c90559
Profeso1012 <itzairso204@gmail.com>
2024-10-04 03:47:25 +0100

    0x01

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      let obj = {'status': 200, 'body': 'Success'};
+      resolve(obj);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
